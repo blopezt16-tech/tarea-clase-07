5e5df58c8bfa8d643938b978940507c345e740bb
Brandon Ovidio Jonathan Lopez Tun <blopezt16@miumg.edu.gt>
2026-09-10 21:10:03 -0600

    Resolver ReporteService.librosNuncaPrestados
    Combina LibroDAO.listarTodos() y PrestamoDAO.listarPrestamosActivosConLibro():
    arma un Set<String> con los titulos que tienen prestamo activo y filtra
    los libros cuyo titulo no esta en ese set. Habilita la opcion 6 del menu.

--- src/main/java/edu/umg/programacion2/clase07/biblioteca/servicio/ReporteService.java
+++ src/main/java/edu/umg/programacion2/clase07/biblioteca/servicio/ReporteService.java
@@ -55,7 +55,20 @@ public class ReporteService {
      */
     public Set<Libro> librosNuncaPrestados() throws SQLException {
         Set<Libro> resultado = new HashSet<>();
-        // TODO: usar libroDAO y prestamoDAO para llenar "resultado" segun las pistas de arriba.
+
+        List<Libro> todosLosLibros = libroDAO.listarTodos();
+        List<PrestamoDetalle> activos = prestamoDAO.listarPrestamosActivosConLibro();
+
+        Set<String> titulosConPrestamoActivo = new HashSet<>();
+        for (PrestamoDetalle detalle : activos) {
+            titulosConPrestamoActivo.add(detalle.getTituloLibro());
+        }
+
+        for (Libro libro : todosLosLibros) {
+            if (!titulosConPrestamoActivo.contains(libro.getTitulo())) {
+                resultado.add(libro);
+            }
+        }
 
         return resultado;
     }
